1a341175c8fd02064357999c59011f8dc9c22062
brintodibyendu <1505092.dbb@ugrad.cse.buet.ac.bd>
2022-05-22 16:36:37 +0600

    Time: 3 ms (34.61%), Space: 5.9 MB (23.83%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,36 +4,24 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long int low=1,high=n;
+        long int low=1;
+        long int high=n;
         long int mid;
-        if(n==1 || isBadVersion(1))
-        {
-            return 1;
-        }
-        if(isBadVersion(n)==true && isBadVersion(n-1)==false)
+        if(n==1)
         {
             return n;
         }
-        while(low<=high)
+        while(low<high)
         {
             mid=(low+high)/2;
             if(isBadVersion(mid)==false)
             {
-                if(isBadVersion(mid+1)==true)
-                {
-                    return mid+1;
-                }
                 low=mid+1;
             }
-            else if(isBadVersion(mid)==true)
-            {
-                if(isBadVersion(mid-1)==false)
-                {
-                    return mid;
-                }
-                high=mid-1;
+            else{
+                high=mid;
             }
         }
-        return -1;
+        return low;
     }
 };
