ad5b7c7df26c46320cac0e915473c24ff95d68a0
brintodibyendu <1505092.dbb@ugrad.cse.buet.ac.bd>
2022-06-02 16:34:51 +0600

    Time: 5 ms (12.86%), Space: 6.5 MB (71.36%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> psc_trn(numRows);
-        for(int i=0;i<numRows;i++)
+        vector<vector<int>> ans(numRows);
+        ans[0] = vector<int>(1);
+        ans[0][0]=1;
+        if(numRows==1)
         {
-            //psc_trn[i]=vector<int>(i+1);
-            psc_trn[i].push_back(1);
-            if(i>1){
-            for(int j=1;j<=i-1;j++)
-            {
-                psc_trn[i].push_back(psc_trn[i-1][j-1]+psc_trn[i-1][j]);
-            }
-            }
-            if(i>=1){
-            psc_trn[i].push_back(1);
-            }
+            return ans;
         }
-        return psc_trn;
+        ans[1] = vector<int>(2);
+        ans[1][0]=1,ans[1][1]=1;
+        if(numRows==2)
+        {
+            return ans;
+        }
+        for(int i=2;i<numRows;i++)
+        {
+            ans[i]=vector<int>(i+1);
+            ans[i][0]=1;
+            for(int j=1;j<i;j++)
+            {
+                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
+            }
+            ans[i][i]=1;
+        }
+        return ans;
     }
 };
